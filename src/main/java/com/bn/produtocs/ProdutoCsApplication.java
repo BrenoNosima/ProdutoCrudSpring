ad951f6245251a3cb2175c9c584923b6d2958924
Nosima <breno.nosima@gmail.com>
2026-03-18 19:51:49 -0300

    first commit

---
+++ src/main/java/com/bn/produtocs/ProdutoCsApplication.java
@@ -0,0 +1,13 @@
+package com.bn.produtocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoCsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdutoCsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bn/produtocs/ProdutoCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.produtocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
