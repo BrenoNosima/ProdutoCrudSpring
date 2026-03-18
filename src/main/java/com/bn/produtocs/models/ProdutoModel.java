39bb09ffcadc8e2efd5444409ff736e70284bb4a
Nosima <breno.nosima@gmail.com>
2026-03-18 20:15:25 -0300

    model criado

---
+++ src/main/java/com/bn/produtocs/models/ProdutoModel.java
@@ -0,0 +1,54 @@
+package com.bn.produtocs.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produto")
+public class ProdutoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "preço")
+    private BigDecimal preco;
+    private Integer quantidadeEmEstoque;
+
+    public ProdutoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(Integer quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+}
